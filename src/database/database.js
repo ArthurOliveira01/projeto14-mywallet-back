0457e104047c192ac8a3774bd276c47fb8d4a3aa
Arthur <arthurods37@gmail.com>
2023-04-25 14:32:36 -0400

    chore: adjust code for deploy on heroku

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,9 @@ import { MongoClient } from "mongodb";
 
 dotenv.config();
 
-const client  = new MongoClient(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017');
+const port = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017';
+
+const client  = new MongoClient(port);
 
 client.connect().then(() => {
     
